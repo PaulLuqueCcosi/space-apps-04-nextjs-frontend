baa14b4f97c59c779905e69d689dca76ade2b7f9
Paul Luque <paul@darshana.ai>
2025-10-04 23:16:41 -0500

    feat: create project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="p-4 space-y-8">
+      <h1 className="text-4xl font-bold mb-6">Ejemplos de Grafos Interactivos</h1>
+
+    </div>
+  );
+}
